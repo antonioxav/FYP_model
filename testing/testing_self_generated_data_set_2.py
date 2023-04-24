ed55e20cf1cb88dc343a71930a0ced1d107e3c67
kgoel1 <kgoel@connect.ust.hk>
2023-04-24 23:11:45 +0800

    File update for Self Generated Data Set 2

---
+++ testing/testing_self_generated_data_set_2.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import random
+import math
+
+close = [1,2,3,4,5,6,7,8,9,10]
+
+close = []
+for i in range(0,20000):
+  x = random.randint(0,1000000000000000000000000000000000000000000000000000)
+  x = float(x)
+  close.append(np.sin(x))
+
+close_df = pd.DataFrame(close)
+
+df_train = close_df.iloc[:int(0.8*close_df.shape[0]),:]
+df_val = close_df.iloc[int(0.8*close_df.shape[0]):int(0.9*close_df.shape[0]),:]
+df_test = close_df.iloc[int(0.9*close_df.shape[0]):,:]
+
+df_train.to_csv('data/processed/test/self_generated_data_set_2_train.csv')
+df_val.to_csv('data/processed/test/self_generated_data_set_2_val.csv')
+df_test.to_csv('data/processed/test/self_generated_data_set_2_test.csv')
